4ad2dcf925c3f5276f17afa0b1edf2208da33fb3
MatthewLeeHan <mlh6fc@virginia.edu>
2019-01-07 18:19:18 -0500

    made slide in menu overlay, need to fix a lot of styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+// const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // set initial state of my menu
@@ -14,7 +14,7 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        // menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         // set menu state
@@ -23,7 +23,7 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        // menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         // set menu state
